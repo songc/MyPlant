549df801ea051c26e158955c3b4a4b667865166c
songchaodevip <songcmail@gmail.com>
2017-09-18 22:49:00 +0800

    添加多配置文件，方便在不同的环境中使用不同的配置文件，方便测试

--- src/test/java/com/songc/dao/imp/HbaseDaoTest.java
+++ src/test/java/com/songc/dao/imp/HbaseDaoTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class HbaseDaoTest {
     @Autowired
     private HbaseDao hbaseDao;
