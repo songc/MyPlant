ff03f766f00299f3bb4ff710b7c12bcf5f138c0e
songchaodevip <songcmail@gmail.com>
2017-12-13 23:32:59 +0800

    进行Double 转double时可能出现空指针异常。通过stream过滤null值。

--- src/main/java/com/songc/core/ap/APExtractService.java
+++ src/main/java/com/songc/core/ap/APExtractService.java
@@ -6,7 +6,9 @@ import com.songc.core.ap.detect.wave.RawWave;
 import com.songc.core.ap.detect.wave.Wave;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by songc on 3/1/2017.
@@ -24,16 +26,12 @@ public class APExtractService {
      * 初始化数据，将字符串数组的数据转换为
      * list<wave>的数据
      *
-     * @return
+     * @return List<wave>
      */
     private List<Wave> init() {
         List<Wave> waveList = new ArrayList<>();
         for (Double[] doubles : this.data) {
-            double[] temp = new double[doubles.length];
-            int i = 0;
-            for (Double aDouble : doubles) {
-                temp[i++] = aDouble;
-            }
+            double[] temp = Arrays.stream(doubles).filter(Objects::nonNull).mapToDouble(s -> s).toArray();
             waveList.add(new RawWave(temp, rate));
         }
         return waveList;
@@ -41,7 +39,7 @@ public class APExtractService {
 
     public List<List<APInRawWave>> getAllAPInfo() {
         List<Wave> rawWaves = this.init();
-        List<List<APInRawWave>> result = new ArrayList<List<APInRawWave>>();
+        List<List<APInRawWave>> result = new ArrayList<>();
         for (Wave rawWave :
                 rawWaves) {
             APWaveExtract apWaveExtract = new APWaveExtract();
